0d4dae743a3718522a6722abe8178ab5745690d9
dangtrantrung <trung280584@gmail.com>
2024-03-30 15:34:35 +0700

    Draw Bounding Boxes

--- AIenv/trackers/player_tracker.py
+++ AIenv/trackers/player_tracker.py
@@ -13,7 +13,7 @@ class PlayerTracker:
         return player_detections
 
     def detect_frame(self,frame):
-        results=self.model.track(frame,persit=True)[0]
+        results=self.model.track(frame,persist=True)[0]
         id_name_dict=results.names
         player_dict={}
         for box in results.boxes:
