bce0668726b1fd1c6bf632257500d05919f14fa7
dangtrantrung <trung280584@gmail.com>
2024-04-06 14:30:41 +0700

    Draw Mini court

--- AIenv/main.py
+++ AIenv/main.py
@@ -1,7 +1,18 @@
-from utils import read_video, save_video
-from trackers import PlayerTracker, BallTracker
+from utils import (read_video,
+                   save_video,
+                   measure_distance,
+                   draw_player_stats,
+                   convert_pixel_distance_to_meters
+                   )
+import constants_court
+from trackers import PlayerTracker,BallTracker
 from court_line_detector import CourtLineDetector
+from mini_court import MiniCourt
 import cv2
+import pandas as pd
+from copy import deepcopy
+
+
 def main():
     #print("hello,world")
     # read video frames
@@ -26,12 +37,17 @@ def main():
 
     # choose player
     player_detections=player_tracker.choose_and_filter_players(court_keypoints,player_detections)
+    # MiniCourt
+    mini_court = MiniCourt(video_frames[0])
 
     # Draw output
         # Draw Player Bounding boxes
     output_video_frames=player_tracker.draw_bboxes(video_frames,player_detections)
     output_video_frames=ball_tracker.draw_bboxes(video_frames,ball_detections)
     output_video_frames=court_line_detector.draw_keypoints_on_video(output_video_frames,court_keypoints)
+
+    # Draw Mini court
+    output_video_frames=mini_court.draw_mini_court(output_video_frames)
     # Draw frame number
     for i,frame in enumerate(output_video_frames):
         cv2.putText(frame,f'Frame number: {i}',(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
